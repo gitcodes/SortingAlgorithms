35a087d4fb20d6d6ca30514bbcb794efaf05ddae
oommen kuruvilla <gitcodes@users.noreply.github.com>
2017-12-15 10:00:28 +0530

    Corrected BubbleSort.cs

--- Bubblesort/BubbleSort.cs
+++ Bubblesort/BubbleSort.cs
@@ -1,31 +1,35 @@
 /*
- * C# Program to Perform Bubble Sort
+
+  Bubble Sort in C#
+
  */
-using System;
+ using System;
+ 
 namespace BubbleSorting
 {
     class Sorting
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            int[] array = {15,11,20,14,12,13,17,16,18,19};  
-            int temp;            
-            for (int j = 0; j <= a.Length - 1; j++)
+            
+            int[] array = { 15, 11, 20, 14, 12, 13, 17, 16, 18, 19 };
+            int temp;
+            for (int j = 0; j < array.Length - 1; j++)
             {
-                for (int i = 0; i <= a.Length - j - 1; i++)
+                for (int i = 0; i < array.Length - j - 1; i++)
                 {
-                    if (a[i] > a[i + 1])
+                    if (array[i] > array[i + 1])
                     {
-                        t = a[i + 1];
-                        a[i + 1] = a[i];
-                        a[i] = t;
+                        temp = array[i + 1];
+                        array[i + 1] = array[i];
+                        array[i] = temp;
                     }
                 }
             }
-            Console.WriteLine("The Sorted Array :");
-            foreach (int aray in a)                         
-                Console.Write(aray + " ");
-            Console.ReadLine();
+            foreach (int element in array)
+                Console.WriteLine(element + " ");
+            
+
         }
     }
 }
